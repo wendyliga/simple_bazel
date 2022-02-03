6fb08ed2211f855b2fb40527c55717a127b0ad88
Wendy Liga <wendy.liga@tokopedia.com>
2022-02-03 13:58:03 +0700

    add test case

---
+++ ios/DummyTest/DummyTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+final class DummyTests: XCTestCase {
+    func test() {
+        XCTAssertTrue(false)
+    }
+    
+    func test_success() {
+        XCTAssertTrue(true)
+    }
+}
