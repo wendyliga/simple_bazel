91c823abb8a8f3e851b1627c9f3158a773dbc638
Wendy Liga <wendy.liga@tokopedia.com>
2022-02-04 01:09:33 +0700

    use NSPrincpleClass

---
+++ ios/DummyTest/UnitTestMain.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestMain.swift
+//  simple_bazel
+//
+//  Created by wendy.liga on 04/02/22.
+//
+
+import Foundation
+import ObjectiveC
+
+@objc
+public final class UnitTestMain: NSObject {
+    public override init() {
+        super.init()
+        print(">>> hello world")
+    }
+}
